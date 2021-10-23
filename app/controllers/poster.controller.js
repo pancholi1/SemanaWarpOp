265c5516d1f34a1fe54cf43f7efe257f2876f789
Francisco Olivero Sagasta <francisco.olivero1998@gmail.com>
2021-10-23 01:51:34 -0300

    endpoints finish

--- app/controllers/poster.controller.js
+++ app/controllers/poster.controller.js
@@ -2,40 +2,8 @@ const db = require("../models");
 const Poster = db.poster;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Tutorial
-exports.create = async (req, res) => {
-  // Validate request
-  if (!req.body.title) {
-    res.status(400).send({
-      message: "Content can not be empty!"
-    });
-    return;
-  }
-  // Create a Tutorial
-  const poster = {
-    title: req.body.title,
-    content: req.body.content,
-    image: req.body.image,
-    category: req.body.category
-  };
-
-  // Save Tutorial in the database
-  try {
-    const newPoster = await Poster.create(poster)
-    return res.send({
-      newPoster
-    });
-  } catch (error) {
-    return res.error(error)
-  }
-
-};
-
 // Retrieve all Tutorials from the database.
 exports.findAll = async (req, res) => {
-  // const title = req.query.title;
-  // var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-
   try {
     const allPoster = await Poster.findAll({
       attributes: [
@@ -47,110 +15,73 @@ exports.findAll = async (req, res) => {
     })
     return res.send(allPoster);
   } catch (error) {
-      return res.send(error);
+    res.status(500).send({
+      message: "Error with the Posters" 
+    });
   }
-
-
 };
 
 // Find a single Tutorial with an id
 exports.findOne = async (req, res) => {
   const id = req.params.id;
-
   try {
     const onePoster = await Poster.findByPk(id)
     res.send(onePoster);
   } catch (error) {
-    res.error(error)
+    res.status(500).send({
+      message: "Error get Poster with id=" + id
+    });
   }
- 
+};
+
+// Create and Save a new Posters
+exports.create = async (req, res) => {
+  try {
+    const newPoster = await Poster.create({
+      ...req.body
+    })
+    return res.send(newPoster);
+  } catch (error) {
+    res.status(500).send({
+      message: "Error creating Poster"
+    });
+  }
 };
 
 // Update a Tutorial by the id in the request
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const id = req.params.id;
-
-  Tutorial.update(req.body, {
+  try {
+    await Poster.update(req.body, {
       where: {
-        id: id
+        id
       }
     })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Tutorial was updated successfully."
-        });
-      } else {
-        res.send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
-        });
-      }
+    res.send({
+      message: "Poster was updated successfully."
     })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
-      });
+  } catch (error) {
+    res.status(500).send({
+      message: "Error updating Poster with id=" + id
     });
+  }
 };
 
 // Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
   const id = req.params.id;
-
-  Tutorial.destroy({
+  try {
+    await Poster.destroy({
       where: {
-        id: id
+        id
       }
     })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Tutorial was deleted successfully!"
-        });
-      } else {
-        res.send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
-        });
-      }
+    return res.send({
+      message: "Tutorial was deleted successfully!"
     })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id
-      });
+  } catch (error) {
+    res.status(500).send({
+      message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
     });
-};
-
-// Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {
-  Tutorial.destroy({
-      where: {},
-      truncate: false
-    })
-    .then(nums => {
-      res.send({
-        message: `${nums} Tutorials were deleted successfully!`
-      });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while removing all tutorials."
-      });
-    });
-};
-
-// find all published Tutorial
-exports.findAllPublished = (req, res) => {
-  Tutorial.findAll({
-      where: {
-        published: true
-      }
-    })
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while retrieving tutorials."
-      });
-    });
+  }
 };
--- app/routes/poster.routes.js
+++ app/routes/poster.routes.js
@@ -3,26 +3,25 @@ module.exports = app => {
 
   var router = require("express").Router();
 
-  // Create a new Tutorial
-  router.post("/", posters.create);
-
   // Retrieve all posters
   router.get("/", posters.findAll);
-
-  // Retrieve all published posters
-  router.get("/published", posters.findAllPublished);
 
   // Retrieve a single Tutorial with id
   router.get("/:id", posters.findOne);
 
+  // Create a new Tutorial
+  router.post("/", posters.create);
+  
   // Update a Tutorial with id
   router.put("/:id", posters.update);
+
+
 
   // Delete a Tutorial with id
   router.delete("/:id", posters.delete);
 
   // Delete all posters
-  router.delete("/", posters.deleteAll);
 
-  app.use('/api/poster', router);
+
+  app.use('/post', router);
 };
